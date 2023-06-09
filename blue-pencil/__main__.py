a38ed330535981271b1a01c0041080c641f3d606
RayMightBeWrong <ruizinho7braga@gmail.com>
2023-06-09 23:25:25 +0100

    Merge

--- blue-pencil/__main__.py
+++ blue-pencil/__main__.py
@@ -14,9 +14,6 @@ def main():
 
 
 
-    
-
-
 if __name__ == '__main__':
     # execute only if run as the entry point into the program
     main()
--- blue-pencil/finder.py
+++ blue-pencil/finder.py
@@ -4,6 +4,7 @@ import spacy
 nlp = spacy.load("pt_core_news_lg")
 
 import re
+# TODO: encontrar morada
 exps = [('email...', r'[a-zA-Z](\w)*@[a-zA-Z](\w)*\.[a-zA-Z](\w)*'), 
         ('código postal...', r'\d{4}-\d{3}'), 
         ('NSS...', r'\d{12}'), ('BI...', r'\d{8}'), 
@@ -11,6 +12,7 @@ exps = [('email...', r'[a-zA-Z](\w)*@[a-zA-Z](\w)*\.[a-zA-Z](\w)*'),
         ('data...', r'\d\d?(-|\/|\.)\d\d?(-|\/|\.)\d\d{1,3}'),
         ('twitter...', r'@\w*'),
         ('www...', r'(http(s)?:\/\/)?(www\.)?[a-zA-z0-9]+\.[a-zA-z0-9]+')]
+# TODO: extensão do website, p.ex., www.youtube.com/ (watch?asdsadasdasdas)
 # telemóvel, NIF e NUS têm o mesmo número de dígitos
 
 def find_entities(text):
@@ -29,16 +31,21 @@ def find_entities(text):
         if matched:
             res.append((w.text, (length, length + len(w.text), exps[exp][0])))
 
+        # TODO: identificar números de acordo com o contexto
         # TODO: add more labels
-        elif w.pos_ == 'PROPN':
-            res.append((w.text, (length, length + len(w.text), w.pos_)))
-        elif w.pos_ == 'NUM':
-            res.append((w.text, (length, length + len(w.text)), w.pos_))
+        elif w.ent_type_ != '':
+            res.append((w.text, (length, length + len(w.text), w.ent_type_)))
+        #elif w.pos_ == 'PROPN':
+        #    res.append((w.text, (length, length + len(w.text), w.pos_)))
+        #elif w.pos_ == 'NUM':
+        #    res.append((w.text, (length, length + len(w.text)), w.pos_))
         #elif w.ent_type_ != '':
         #    pass
             #print('entity found: ' + w.text + ' | ' + str(w.ent_type_))
         length += len(w.text)
         length += len(w.whitespace_)
+    print(res)
 
-#rfile = open('test', 'r')
-#text = rfile.read()
+rfile = open('test', 'r')
+text = rfile.read()
+find_entities(text)
